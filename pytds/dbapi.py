fef4ee7f0764328bbbaa3cb0606f76ccfb21da3c
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-02-26 09:51:17 -0500

    version 1.1.3

--- pytds/dbapi.py
+++ pytds/dbapi.py
@@ -2,7 +2,7 @@
 """DB-SIG compliant module for communicating with MS SQL servers"""
 
 __author__ = 'Mikhail Denisenko <denisenkom@gmail.com>'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 import logging
 import decimal
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(name='python-tds',
-        version='1.1.2',
-        description='Python DBAPI driver for MSSQL using pure Python TDS (Tabular Data Stream) protocol implementation',
-        author='Mikhail Denisenko',
-        author_email='denisenkom@gmail.com',
-        url='https://github.com/denisenkom/pytds',
-        packages=['pytds'],
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-        ],
-        zip_safe=True,
-        install_requires=['python-dateutil'],
-        )
+      version='1.1.3',
+      description='Python DBAPI driver for MSSQL using pure Python TDS (Tabular Data Stream) protocol implementation',
+      author='Mikhail Denisenko',
+      author_email='denisenkom@gmail.com',
+      url='https://github.com/denisenkom/pytds',
+      packages=['pytds'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+      ],
+      zip_safe=True,
+      install_requires=['python-dateutil'],
+      )
