4f86935bc6c8ebb4443d07bf254b457ed7690131
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-07-02 17:20:06 -0400

    bumped version to 1.5.4

--- pytds/dbapi.py
+++ pytds/dbapi.py
@@ -1,7 +1,7 @@
 """DB-SIG compliant module for communicating with MS SQL servers"""
 
 __author__ = 'Mikhail Denisenko <denisenkom@gmail.com>'
-__version__ = '1.5.3'
+__version__ = '1.5.4'
 
 import logging
 import six
