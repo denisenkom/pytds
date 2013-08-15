dd551440612763407767623a36e1c76dd3f5897f
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-08-15 00:48:57 -0400

    using old python unittest

--- ci.sh
+++ ci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 $PYTHONHOME/bin/virtualenv env
 . env/bin/activate
 pip install pydes --use-mirrors
 pip install coverage --use-mirrors
 pip install nose --use-mirrors
-nosetests --with-coverage --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
+python -m unittest discover
