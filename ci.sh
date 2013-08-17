2f8595a38c9bab4f8a1c4efead8871617bf67020
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-08-17 18:16:50 -0400

    added installation of python 2.6 requirements into ci.sh and ci.bat

--- ci.sh
+++ ci.sh
@@ -5,4 +5,5 @@ pip install pydes --use-mirrors
 pip install coverage --use-mirrors
 pip install -r requirements.txt --use-mirrors
 pip install -r test_requirements.txt --use-mirrors
+pip install -r test_requirements26.txt --use-mirrors; true
 nosetests --with-coverage --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
