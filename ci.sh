3d996759d8399d51607f60e45db3fbf7ee21288d
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-08-21 19:42:34 -0400

    using nosetests instead of standard python unittest to get junitxml and coverage

--- ci.sh
+++ ci.sh
@@ -6,4 +6,4 @@ pip install pydes --use-mirrors
 pip install coverage --use-mirrors
 pip install nose --use-mirrors
 pip install -e .
-python -m unittest discover
+nosetests --with-coverage --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
