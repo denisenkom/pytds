a172780fc582430d01ab7922bd6ab0c4fa4f1461
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-09-12 17:12:05 -0400

    use nosetests for windows testing

--- ci.bat
+++ ci.bat
@@ -4,4 +4,4 @@ pip install . --use-mirrors
 pip install pydes --use-mirrors
 pip install coverage --use-mirrors
 pip install nose --use-mirrors
-python -m unittest discover
+nosetests --with-coverage --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
