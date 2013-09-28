235a2fb192d42cc87c99c7b6d271deef745c9c88
Mikhail Denisenko <denisenkom@gmail.com>
2013-09-27 22:32:59 -0400

    erase coverage info from previous builds

--- ci.bat
+++ ci.bat
@@ -5,4 +5,4 @@ pip install coverage --use-mirrors
 pip install -r requirements.txt --use-mirrors
 pip install -r test_requirements.txt --use-mirrors
 pip install -r test_requirements26.txt --use-mirrors
-nosetests --with-coverage --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
+nosetests --with-coverage --cover-erase --cover-package=pytds --cover-xml --cover-xml-file=coverage.xml --with-xunit --xunit-file=xunit.xml
