c0958bc187fe1c608f7823e1a80957884835e72b
Mikhail Denisenko <denisenk@amazon.com>
2016-04-09 12:29:27 -0400

    testing: fewer env variables are required
    DATABASE, SQLUSER, SQLPASSWORD environment variables are now optional

--- tests/settings.py
+++ tests/settings.py
@@ -6,28 +6,29 @@ CONNECT_KWARGS = {}
 LIVE_TEST = 'HOST' in os.environ
 if LIVE_TEST:
     HOST = os.environ['HOST']
-    DATABASE=os.environ['DATABASE']
-    USER=os.environ['SQLUSER']
-    PASSWORD=os.environ['SQLPASSWORD']
+    DATABASE = os.environ.get('DATABASE', 'test')
+    USER = os.environ.get('SQLUSER', 'sa')
+    PASSWORD = os.environ.get('SQLPASSWORD', 'sa')
     USE_MARS = bool(os.environ.get('USE_MARS', True))
     SKIP_SQL_AUTH = bool(os.environ.get('SKIP_SQL_AUTH'))
 
     import pytds
 
     CONNECT_KWARGS = {
-            'server': HOST,
-            'database': DATABASE,
-            'user': USER,
-            'password': PASSWORD,
-            'use_mars': USE_MARS,
-            'bytes_to_unicode': True,
-            }
+        'server': HOST,
+        'database': DATABASE,
+        'user': USER,
+        'password': PASSWORD,
+        'use_mars': USE_MARS,
+        'bytes_to_unicode': True,
+    }
 
     if 'tds_version' in os.environ:
         CONNECT_KWARGS['tds_version'] = getattr(pytds, os.environ['tds_version'])
 
     if 'auth' in os.environ:
         import pytds.login
+
         CONNECT_KWARGS['auth'] = getattr(pytds.login, os.environ['auth'])()
 
     if 'bytes_to_unicode' in os.environ:
