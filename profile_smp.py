104ea6b3996f271bb474f0dc7afc2f2359f6419e
Mikhail Denisenko <denisenk@amazon.com>
2017-12-12 18:47:30 -0500

    Profiling code for SMP

---
+++ profile_smp.py
@@ -0,0 +1,50 @@
+import struct
+import cProfile
+import pstats
+import io
+
+import pytds.smp
+
+transport = None
+
+bufsize = 512
+smp_header = struct.Struct('<BBHLLL')
+
+
+class Sock:
+    def __init__(self):
+        self._read_pos = 0
+        self._seq = 1
+        self._buf = bytearray(b'\x00' * bufsize)
+        smp_header.pack_into(self._buf, 0, 0x53, 0x8, 0, bufsize, self._seq, 4)
+
+    def sendall(self, data, flags=0):
+        pass
+
+    def recv(self, size):
+        if self._read_pos >= len(self._buf):
+            self._seq += 1
+            smp_header.pack_into(self._buf, 0, 0x53, 0x8, 0, bufsize, self._seq, 4)
+            self._read_pos = 0
+        res = self._buf[self._read_pos:self._read_pos + size]
+        self._read_pos += len(res)
+        return res
+
+    def close(self):
+        pass
+
+
+sock = Sock()
+mgr = pytds.smp.SmpManager(transport=sock)
+sess = mgr.create_session()
+pr = cProfile.Profile()
+pr.enable()
+for _ in range(50000):
+    sess.recv(bufsize)
+pr.disable()
+sortby = 'tottime'
+ps = pstats.Stats(pr).sort_stats(sortby)
+ps.print_stats()
+
+# original 0.7 sec
+# not using readall  0.6 sec
