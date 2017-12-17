6ec0d20a4b8071996da0232d3ff87878a850f2d2
Mikhail Denisenko <denisenk@amazon.com>
2017-12-17 10:41:10 -0500

    Fixed cancellation handling

--- src/pytds/tds.py
+++ src/pytds/tds.py
@@ -250,7 +250,7 @@ class _TdsReader(object):
         try:
             pos = 0
             while pos < _header.size:
-                received = self._transport.recv_into(self._bufview[pos:])
+                received = self._transport.recv_into(self._bufview[pos:_header.size-pos])
                 if received == 0:
                     raise tds_base.ClosedConnectionError()
                 pos += received
