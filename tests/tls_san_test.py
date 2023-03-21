c9b929fe9199145d5fafff6f6a4494ad9c5f91f8
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2023-03-21 08:33:49 +0100

    fix tests

--- tests/tls_san_test.py
+++ tests/tls_san_test.py
@@ -4,7 +4,7 @@ def test_san():
     assert not is_san_matching("", "host.com")
     assert is_san_matching("database.com", "database.com")
     assert not is_san_matching("notdatabase.com", "database.com")
-    assert is_san_matching("*.database.com", "database.com")
-    assert not is_san_matching("*.database.com", "*.database.com")
+    assert not is_san_matching("*.database.com", "database.com")
+    assert is_san_matching("*.database.com", "test.database.com")
     assert not is_san_matching("database.com", "*.database.com")
     assert not is_san_matching("test.*.database.com", "test.subdomain.database.com") # That star should be at first position
