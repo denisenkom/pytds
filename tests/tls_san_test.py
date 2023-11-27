dc3d1889738b0b7adf4dc8ae35c211320a115b4d
terratrue-daniel <97548386+terratrue-daniel@users.noreply.github.com>
2023-11-27 15:15:02 -0800

    Fix parsing multiple SANs (#155)

--- tests/tls_san_test.py
+++ tests/tls_san_test.py
@@ -8,3 +8,9 @@ def test_san():
     assert is_san_matching("*.database.com", "test.database.com")
     assert not is_san_matching("database.com", "*.database.com")
     assert not is_san_matching("test.*.database.com", "test.subdomain.database.com") # That star should be at first position
+    # test stripping DNS:
+    assert is_san_matching("DNS:westus2-a.control.database.windows.net", "westus2-a.control.database.windows.net")
+    assert is_san_matching("DNS:*.database.windows.net", "my-sql-server.database.windows.net")
+    # test parsing multiple SANs
+    assert is_san_matching("DNS:westus2-a.control.database.windows.net,DNS:*.database.windows.net", "my-sql-server.database.windows.net")
+    assert is_san_matching("DNS:westus2-a.control.database.windows.net, DNS:*.database.windows.net", "my-sql-server.database.windows.net")
