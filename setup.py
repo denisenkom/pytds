fb70d5968d6ca5ebe1fbc9c99a81e0841dabad46
Mikhail Denisenko <denisenk@amazon.com>
2024-09-27 23:18:15 -0400

    Remove Python 3.13 from supported versions, since it is not yet released

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
     ],
     zip_safe=True,
     install_requires=requirements,
